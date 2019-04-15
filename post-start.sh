f92e952a8744baf1ad5e3c596fc2d94820964c0c
Piotr Stawarski <piotr.stawarski@zerodowntime.pl>
2019-04-15 16:22:30 +0200

    lifecycle

--- post-start.sh
+++ post-start.sh
@@ -1,3 +1,27 @@
-#!/bin/sh
+#!/bin/sh -e
+
+if [ -z ${HOSTNAME+x} ]; then
+  HOSTNAME=$(hostname -s)
+fi
+
+if [ -z "$ELASTICSEARCH_NODE_NAME" ]; then
+  ELASTICSEARCH_NODE_NAME=$(curl -s -X GET localhost:9200/ | jq -r .name)
+fi
+
+exclude_name=$(curl -s -X GET "http://localhost:9200/_cluster/settings" | jq -r .transient.cluster.routing.allocation.exclude._name)
+if [ "$exclude_name" == "null" ]; then
+  exclude_name=""
+fi
+
+exclude_name=",${exclude_name},"
+while [ 1 ]; do
+  fixed="${exclude_name/,$ELASTICSEARCH_NODE_NAME,/,}"
+  [ "$exclude_name" != "$fixed" ] || break
+  exclude_name="$fixed"
+done
+exclude_name=$(echo $exclude_name | sed 's/^,*//' | sed 's/,*$//')
+
+curl -s -XPUT -H 'Content-Type: application/json' 'localhost:9200/_cluster/settings' \
+  -d '{ "transient": {"cluster.routing.allocation.exclude._name": "'$exclude_name'" } }'
 
 exit 0
--- pre-stop.sh
+++ pre-stop.sh
@@ -1,3 +1,30 @@
-#!/bin/sh
+#!/bin/sh -e
+
+if [ -z ${HOSTNAME+x} ]; then
+  HOSTNAME=$(hostname -s)
+fi
+
+if [ -z "$ELASTICSEARCH_NODE_NAME" ]; then
+  ELASTICSEARCH_NODE_NAME=$(curl -s -X GET localhost:9200/ | jq -r .name)
+fi
+
+exclude_name=$(curl -s -X GET "http://localhost:9200/_cluster/settings" | jq -r .transient.cluster.routing.allocation.exclude._name)
+if [ "$exclude_name" == "null" ]; then
+  exclude_name=""
+fi
+
+exclude_name="${ELASTICSEARCH_NODE_NAME},${exclude_name}"
+
+curl -s -XPUT -H 'Content-Type: application/json' 'localhost:9200/_cluster/settings' \
+  -d '{ "transient": {"cluster.routing.allocation.exclude._name": "'$exclude_name'" } }'
+
+while [ 1 ]; do
+  allocation=$(curl -s -GET 'http://localhost:9200/_cat/allocation?format=json' | jq -r --arg this "$ELASTICSEARCH_NODE_NAME" '.[] | select(.node == $this).shards')
+  if [ "$allocation" -eq "0" ]; then
+    break;
+  fi
+  echo "$allocation shards remaining.."
+  sleep 5
+done
 
 exit 0
